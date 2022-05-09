ed34bfaf346f898c655a92e881a806fb3fe98463
Jack Sun <jsun1590@gmail.com>
2022-05-09 13:20:46 +0800

    remove newline

--- project_files/Item.java
+++ project_files/Item.java
@@ -1,3 +1,4 @@
+
 /**
  * Represents a "Thing" that will be hidden on the board by the AIPlayer.
  *
@@ -13,8 +14,8 @@ import java.util.Arrays;
 public class Item {
 
     private String type; // a string representing the type of the object, i.e. "phone" or "book".
-    private int[][] shape; // a 2D array representing the shape of the object. hint: you can assume that 
-                           //this is a square array where 1 represents an item piece and
+    private int[][] shape; // a 2D array representing the shape of the object. hint: you can assume that
+                           // this is a square array where 1 represents an item piece and
                            // 0 represent an empty space.
     private Integer locationX; // the x location of of the shape (i.e. shape[0][0] is at locationX).
     private Integer locationY; // the x location of of the shape (i.e. shape[0][0] is at locationY).
@@ -27,11 +28,11 @@ public class Item {
      */
     public Item(String type, int[][] shape) {
         // TODO 1
-        
     }
 
     /**
      * Returns the type of the item
+     * 
      * @return the type of the item
      */
     public String getType() {
@@ -41,6 +42,7 @@ public class Item {
 
     /**
      * Returns the shape of the item
+     * 
      * @return the shape of the item
      */
     public int[][] getShape() {
@@ -50,6 +52,7 @@ public class Item {
 
     /**
      * Sets the shape of an iem to a new shape
+     * 
      * @return the shape
      */
     public void setShape(int[][] newShape) {
@@ -65,6 +68,7 @@ public class Item {
 
     /**
      * Returns whether or not the item has been found by the player
+     * 
      * @return true if the item has been found, false otherwise
      */
     public boolean getIsFound() {
@@ -74,6 +78,7 @@ public class Item {
 
     /**
      * Returns the x location of the item
+     * 
      * @return the x location of the item
      */
     public int getLocationX() {
@@ -83,6 +88,7 @@ public class Item {
 
     /**
      * Returns the y location of the item
+     * 
      * @return the y location of the item
      */
     public int getLocationY() {
@@ -92,6 +98,7 @@ public class Item {
 
     /**
      * Sets the x and y locations for the item.
+     * 
      * @param xloc the x location
      * @param yloc the y location
      */
@@ -100,7 +107,8 @@ public class Item {
     }
 
     /**
-     * Rotates the item (i.e. its shape) 90 degrees clockwise and sets the shape of the item as the
+     * Rotates the item (i.e. its shape) 90 degrees clockwise and sets the shape of
+     * the item as the
      * new rotated array.
      */
     public void rotate90Degrees() {
